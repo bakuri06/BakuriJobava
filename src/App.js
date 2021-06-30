7cc4c401b667f28f1c6b92dd07375250179bf2a2
bakuri06 <b.jobava1@gmail.com>
2021-06-30 18:58:27 +0400

    material

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import SignupForm from './Formik/Form';
 import './App.css'
+import Bar from './Layout/Header/Bar';
+import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import FirstContent from './Layout/FirstContent/Content'
+import MainContent from './Layout/MainContent/MainContent';
 
 const App = () => {
-  return(
-    <div className='main-container'>
-      <SignupForm />
+  const useStyles = makeStyles({
+    root: {
+      paddingLeft: "10px",
+      paddingRight: "10px",
+    }
+  });
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Container classes={{ root: classes.root }} maxWidth="xl">
+        <Bar />
+      </Container>
+      <>
+        <Container maxWidth="xl">
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <FirstContent />
+            </Grid>
+            <MainContent />
+          </Grid>
+        </Container>
+      </>
     </div>
   )
 };
