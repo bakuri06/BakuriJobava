a227b2a5f2156d84de0cd3e10a744f17f656bb64
bakuri06 <b.jobava1@gmail.com>
2021-06-30 19:18:08 +0400

    material

--- src/Layout/MainContent/MainContent.js
+++ src/Layout/MainContent/MainContent.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Card from './Card'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     root: {
         flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
+    }
 }));
 
 export default function MainContent() {
     const classes = useStyles();
-    
+
     return (
         <div className={classes.root}>
             <Grid container spacing={1}>
